b81f41f79fdc7c7b99b2200236b7d36f9919cc8d
Ghenadie Berco <berco.ghenadie@fortis-cs.com>
2025-08-15 09:55:40 -0400

    initial tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import './App.css'
+import "./App.css";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { useState } from "react";
 
-function App() {
-
-  return (
-    <>
-      Work in progress...
-    </>
-  )
+export interface Tab {
+  id: string;
+  title: string;
 }
 
-export default App
+const tabs: Tab[] = [
+  { id: "about", title: "About" },
+  { id: "projects", title: "Projects" },
+  { id: "tech-skills", title: "Tech Skills" },
+  { id: "contact", title: "Contact" },
+];
+
+function App() {
+  const [selectedTab, setSelectedTab] = useState(tabs[0].id);
+
+  const handleChange = (_event: React.SyntheticEvent, tabId: string) => {
+    setSelectedTab(tabId);
+  };
+
+  return (
+    <Tabs
+      value={selectedTab}
+      onChange={handleChange}
+      className="zzz"
+      aria-label="basic tabs example"
+    >
+      {tabs.map((tab) => (
+        <Tab key={tab.id} label={tab.title} value={tab.id}></Tab>
+      ))}
+    </Tabs>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import CssBaseline from "@mui/material/CssBaseline";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <CssBaseline>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </CssBaseline>
+);
